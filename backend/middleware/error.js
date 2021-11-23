cc2a671d4bdf131b91fe8fa1dd1e4f532e949e87
NAVEEN KAMATH <naveen.kamath002@gmail.com>
2021-11-23 14:59:16 +0530

    Backend user and Password Authentication

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -8,6 +8,23 @@ module.exports = (err, req, res, next) => {
     const message = `Resource Not Found. Invalid ${err.path}`;
     err = new ErrorHandler(message, 400);
   }
+  // Mongoose Duplicate key Error
+  if (err.code === 11000) {
+    const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  //Wrong Jwt error
+  if (err.name === `JsonWebTokenError`) {
+    const message = `Json web Token is invalid , Try again`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  // Jwt expire error
+  if (err.name === `TokenExpiredError`) {
+    const message = `Json token Expired ,Try again`;
+    err = new ErrorHandler(message, 400);
+  }
 
   res.status(err.statusCode).json({ success: false, message: err.message });
 };
